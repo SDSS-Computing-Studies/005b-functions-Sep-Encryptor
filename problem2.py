462422d17fb07704451f53ea303f6c6ea1febea2
Sep-Encryptor <sepehrz867@deltalearns.ca>
2021-03-05 01:08:27 -0800

    done

--- problem2.py
+++ problem2.py
@@ -9,3 +9,9 @@ Note that the coordinates should be signed (positive or negative) floats
 """
 import math
 
+def distance(a, b):
+    side1 = a[0] - b[0]
+    side2 = a[1] - b[1]
+    answer = math.sqrt(( side1 ** 2 + side2 ** 2))
+    return answer
+    
